3aff7210302cc38f06a0d57507a064f26e79634a
vidyashree33 <vidyashreehsgowda@gmail.com>
2026-03-31 12:09:17 +0530

    task 30-03-2026

---
+++ Calculator.java
@@ -0,0 +1,96 @@
+class Calculator{
+
+ public void addofNumbers(int a,int b){
+ System.out.println(a+b);
+ }
+ public void addofNumbers(int a,int b,int c){
+   System.out.println(a+b+c);
+ }
+ public void addofNumbers(int a,int b,int c,int d){
+ System.out.println(a+b+c+d);
+ }
+ public void addofNumbers(int a,int b,int c,int e,int t){
+ System.out.println(a+b+c+e+t);
+ }
+ public void addofNumbers(int a,int b,int c,int d,int e,int f){
+ System.out.println(a+b+c+d+e+f);
+ }
+ public void addofNumbers(int a,int b,int c,int d,int e,int f,int g){
+ System.out.println(a+b+c+d+e+f+g);
+ }
+ public void addofNumbers(int a,int b,int c,int d,int e,int f,int g,int h){
+ System.out.println(a+b+c+d+e+f+g+h);
+ }
+ public void addofNumbers(int a,int b,int c,int d,int e,int f,int g,int h,int i){
+ System.out.println(a+b+c+d+e+f+g+h+i);
+ }
+ public void addofNumbers(int a,int b,int c,int d,int e,int f,int g,int h,int i,int j){
+ System.out.println(a+b+c+d+e+f+g+h+i+j);
+ }
+ public void addofNumbers(int a,int b,int c,int d,int e,int f,int g,int h,int i,int j,int k){
+ System.out.println(a+b+c+d+e+f+g+h+i+j+k);
+ }
+ 
+  public void substractionoFNumber(int a,int b){
+ System.out.println(a-b);
+ }
+ public void substractionoFNumber(int a,int b,int c){
+   System.out.println(a-b-c);
+ }
+ public void substractionoFNumber(int a,int b,int c,int d){
+ System.out.println(a-b-c-d);
+ }
+ public void substractionoFNumber(int a,int b,int c,int e,int t){
+ System.out.println(a-b-c-e-t);
+ }
+ public void substractionoFNumber(int a,int b,int c,int d,int e,int f){
+ System.out.println(a-b-c-d-e-f);
+ }
+ public void substractionoFNumber(int a,int b,int c,int d,int e,int f,int g){
+ System.out.println(a-b-c-d-e-f-g);
+ }
+ public void substractionoFNumber(int a,int b,int c,int d,int e,int f,int g,int h){
+ System.out.println(a-b-c-d-e-f-g-h);
+ }
+ public void substractionoFNumber(int a,int b,int c,int d,int e,int f,int g,int h,int i){
+ System.out.println(a-b-c-d-e-f-g-h-i);
+ }
+ public void substractionoFNumber(int a,int b,int c,int d,int e,int f,int g,int h,int i,int j){
+ System.out.println(a-b-c-d-e-f-g-h-i-j);
+ }
+ public void substractionoFNumber(int a,int b,int c,int d,int e,int f,int g,int h,int i,int j,int k){
+ System.out.println(a-b-c-d-e-f-g-h-i-j-k);
+ }
+ 
+ public void multiplicationoFNumber(int a,int b){
+ System.out.println(a*b);
+ }
+ public void multiplicationoFNumber(int a,int b,int c){
+   System.out.println(a*b*c);
+ }
+ public void multiplicationoFNumber(int a,int b,int c,int d){
+ System.out.println(a*b*c*d);
+ }
+ public void multiplicationoFNumber(int a,int b,int c,int e,int t){
+ System.out.println(a*b*c*e-t);
+ }
+ public void multiplicationoFNumber(int a,int b,int c,int d,int e,int f){
+ System.out.println(a*b*c*d*e*f);
+ }
+ public void multiplicationoFNumber(int a,int b,int c,int d,int e,int f,int g){
+ System.out.println(a*b*c*d*e*f*g);
+ }
+ public void multiplicationoFNumber(int a,int b,int c,int d,int e,int f,int g,int h){
+ System.out.println(a*b*c*d*e*f*g*h);
+ }
+ public void multiplicationoFNumber(int a,int b,int c,int d,int e,int f,int g,int h,int i){
+ System.out.println(a*b*c*d*e*f*g*h*i);
+ }
+ public void multiplicationoFNumber(int a,int b,int c,int d,int e,int f,int g,int h,int i,int j){
+ System.out.println(a*b*c*d*e*f*g*h*i*j);
+ }
+ public void multiplicationoFNumber(int a,int b,int c,int d,int e,int f,int g,int h,int i,int j,int k){
+ System.out.println(a*b*c*d*e*f*g*h*i*j*k);
+ }
+ }
+ 
